64b35f1d03d732634f998057ea94b5d34e1ea849
Atte <atte@berkeley.edu>
2024-07-26 15:39:21 -0700

    Add merge sort example to test file

--- app/validate.py
+++ app/validate.py
@@ -7,7 +7,10 @@ def number_operand(token: Token, operand: LoxObject) -> float:
     if isinstance(operand, float):
         return operand
 
-    raise LoxRuntimeError(token, f"Operand to {token.lexeme} must be a number.")
+    raise LoxRuntimeError(
+        token,
+        f"Operand to {token.lexeme} must be a number. Got {operand=} ({type(operand)=})",
+    )
 
 
 def number_operands(
@@ -15,8 +18,13 @@ def number_operands(
 ) -> tuple[float, float]:
     if isinstance(left, float) and isinstance(right, float):
         return left, right
+    if isinstance(left, int) and isinstance(right, int):
+        return float(left), float(right)
 
-    raise LoxRuntimeError(token, f"Operands to {token.lexeme} must be numbers.")
+    raise LoxRuntimeError(
+        token,
+        f"Operands to {token.lexeme} must be numbers. Got {left=} {right=} ({type(left)=} {type(right)=})",
+    )
 
 
 def number_or_string_operands(
@@ -24,9 +32,12 @@ def number_or_string_operands(
 ) -> tuple[float, float] | tuple[str, str]:
     if isinstance(left, float) and isinstance(right, float):
         return left, right
+    if isinstance(left, int) and isinstance(right, int):
+        return float(left), float(right)
     if isinstance(left, str) and isinstance(right, str):
         return left, right
 
     raise LoxRuntimeError(
-        token, f"Operands to {token.lexeme} must be numbers or strings."
+        token,
+        f"Operands to {token.lexeme} must be numbers or strings. Got {left=} {right=} ({type(left)=} {type(right)=})",
     )
